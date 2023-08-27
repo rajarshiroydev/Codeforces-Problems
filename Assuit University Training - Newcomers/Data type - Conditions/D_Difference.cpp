75fe60d7611a0ca23e2606decbfd9e382a6f0083
Rajarshi Roy <roystark.dev@gmail.com>
2023-08-27 11:30:55 +0530

    Removed a folder

--- Assuit University Training - Newcomers/Data type - Conditions/D_Difference.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-int main() {
-    long long a, b, c, d; cin >> a >> b >> c >> d;
-    cout << "Difference = " << (a * b) - (c * d);
-    return 0;
-}
